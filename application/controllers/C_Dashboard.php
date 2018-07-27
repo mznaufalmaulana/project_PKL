d202e7263351b68e9a127ba8492fbf7629dcec75
MZ. Naufal Maulana <mznaufalmaulana@gmail.com>
2018-07-27 17:06:13 +0700

    show doctor

--- application/controllers/C_Dashboard.php
+++ application/controllers/C_Dashboard.php
@@ -23,11 +23,22 @@ class C_Dashboard extends CI_Controller
 		$retVal ['data_kota']= $this->m_doctor->getCategory_City();
 		$retVal ['data_spesialis']= $this->m_doctor->getCategory_Spesialis();
 
-		if (is_null($retVal['data_dokter']['imgAvatar']))
-		{
-			$retVal['data_dokter']['imgAvatar'] = '<img src="echo base_url("/theme/img/user_default.png")"';
-		}
+		// if (is_null($retVal['data_dokter']['imgAvatar']))
+		// {
+		// 	$retVal['data_dokter']['imgAvatar'] = '<img src="echo base_url("/theme/img/user_default.png")"';
+		// }
 		$this->load->view('Dashboard/content', $retVal);
+	}
+	function viewDokter()
+	{
+		$dt = array(
+				"txtKeyword" => "0",
+				"intIDKota" => $this->input->post("kota"),
+				"intIDSpesialisDokter" => $this->input->post("spesialis"),
+				"intIDJenisKelamin" => $this->input->post("jk_group")
+			);
+		$data = $this->m_doctor->show_doctor($dt)->result();
+		echo json_encode($data);
 	}
 }
  ?>
--- application/views/Dashboard/Dokter.php
+++ application/views/Dashboard/Dokter.php
@@ -44,18 +44,38 @@
 
                 <div class="form-group">
                   <div class="col-lg-10 col-lg-offset-2">
-                    <button type="done" class="btn btn-primary" id="btnSubmit">Sorting</button>
+                    <button type="done" class="btn btn-primary" id="btnSubmit" onclick="viewDokter()">Sorting</button>
                   </div>
                 </div> <!-- Akhir Tombol -->
-</form>
+            </form>
 
-<?php 
+            <!-- <div class="row">
+              <table class="table table-striped" id="dataDokter">
+                <thead>
+                <tr>
+                    <th>Kode</th>
+                    <th>Nama Barang</th>
+                    <th>Harga</th>
+                </tr>
+            </thead>
+                <tbody id="show_data">
+                  
+                </tbody>
+              </table>
+            </div> -->
+
+        <?php 
           foreach ($data_dokter as $key => $value)
           {
             echo "<table border=\"0\">";
               echo "<tr>";
                 echo "<td>";
-                  echo "<img src=\" ". $value['imgAvatar'] ."\">";
+                  if (is_null($value['imgAvatar'])) {
+                    echo "<img class=\" img-circle\" src=\" ". BASE_THEME. '/img/img1.jpg' ." \">";
+                  }
+                  else {
+                    echo "<img class=\" img-circle\" src=\" ". $value['imgAvatar'] ."\">";
+                  }
                 echo "</td>";
                 echo "<td>";
                   echo "<h3><b>".$value['txtNamaDokter']."</b></h3>";
@@ -69,3 +89,35 @@
             echo "</table>";
           }
         ?>
+
+        <script type="text/javascript">
+          // f$(document).ready(function(){
+          //     tampil_data_barang();   //pemanggilan fungsi tampil barang.
+               
+          //     $('#dataDokter').dataTable();
+                
+          //     //fungsi tampil barang
+          //     function tampil_data_barang(){
+          //         $.ajax({
+          //             type  : 'ajax',
+          //             url   : '<?php echo base_url()?>/c_dashboard/viewDokter',
+          //             async : false,
+          //             dataType : 'json',
+          //             success : function(data){
+          //                 var html = '';
+          //                 var i;
+          //                 for(i=0; i<data.length; i++){
+          //                     html += '<tr>'+
+          //                             '<td>'+data[i].txtNamaDokter+'</td>'+
+          //                             '<td>'+data[i].txtNoHP+'</td>'+
+          //                             '<td>'+data[i].txtAlamat+'</td>'+
+          //                             '</tr>';
+          //                 }
+          //                 $('#show_data').html(html);
+          //             }
+       
+          //         });
+          //     }
+       
+          // });
+        </script>
